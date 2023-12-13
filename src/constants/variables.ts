cb7b6bb8332fc843b7e6e827ef2e400ddc3456bb
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2023-12-13 08:49:25 +0000

    Update feePerGas to 10 gwei

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -7,7 +7,7 @@ export class Variables {
   static SOCKET_SERVER_URL = process.env.SOCKET_SERVER_URL ?? 'wss://ws.fuse.io/connection/websocket';
   static ETHERSPOT_FACTORY = process.env.ETHERSPOT_FACTORY ?? '0x7f6d8F107fE8551160BD5351d5F1514A6aD5d40E';
   static DEFAULT_TX_OPTIONS = {
-    feePerGas: '1000000',
+    feePerGas: '10000000000',
     feeIncrementPercentage: 10,
     withRetry: false,
   };
