3ddc27e8c4048ad2ad1f11e3fcb0043fb472b670
Lior Agnin <lior.agnin@gmail.com>
2024-01-17 14:45:21 +0200

    Introduced a new property to the txOptions object, customNonceKey, which gives account holders greater control over the semi-abstracted nonce system.

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -6,7 +6,7 @@ export class Variables {
   static BASE_URL = process.env.BASE_URL ?? 'api.fuse.io';
   static SOCKET_SERVER_URL = process.env.SOCKET_SERVER_URL ?? 'wss://ws.fuse.io/connection/websocket';
   static ETHERSPOT_FACTORY = process.env.ETHERSPOT_FACTORY ?? '0x7f6d8F107fE8551160BD5351d5F1514A6aD5d40E';
-  static DEFAULT_TX_OPTIONS = {
+  static DEFAULT_TX_OPTIONS: DefaultTxOptions = {
     feePerGas: '10000000000',
     feeIncrementPercentage: 10,
     withRetry: false,
