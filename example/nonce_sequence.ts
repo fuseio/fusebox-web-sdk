42326afe3aec4c368f66a93e964bead86fa20590
Lior Agnin <lior.agnin@gmail.com>
2024-01-17 14:48:02 +0200

    oops

--- example/nonce_sequence.ts
+++ example/nonce_sequence.ts
@@ -14,8 +14,8 @@ const main = async () => {
   const value = parseEther("0.001");
   const data = Uint8Array.from([]);
   const txOptions = { ...Variables.DEFAULT_TX_OPTIONS, useNonceSequence: true };
-  await fuseSDK.callContract(to, value, data, {...txOptions, customNonceKey: 1000});
-  await fuseSDK.callContract(to, value, data, {...txOptions, customNonceKey: 184467440737});
+  await fuseSDK.callContract(to, value, data, txOptions);
+  await fuseSDK.callContract(to, value, data, txOptions);
 };
 
 main();
