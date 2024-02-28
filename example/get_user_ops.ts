c025f7bc9d53fb9a73484c7be2997ce8662ca3ee
Lior Agnin <lior.agnin@gmail.com>
2024-02-28 21:51:50 +0200

    m

--- example/get_user_ops.ts
+++ example/get_user_ops.ts
@@ -4,7 +4,7 @@ import { FuseSDK, UserOp } from '../src';
 const main = async () => {
   const credentials = new ethers.Wallet(process.env.PRIVATE_KEY as string);
   const publicApiKey = process.env.PUBLIC_API_KEY as string;
-  const fuseSDK = await FuseSDK.init(publicApiKey, credentials, { withPaymaster: true, baseUrl: 'api.staging.fuse.io' });
+  const fuseSDK = await FuseSDK.init(publicApiKey, credentials, { withPaymaster: true });
   const userOperations: UserOp[] = await fuseSDK.graphQLModule.getUserOpsBySender(fuseSDK.wallet.getSender())
   for (const action of userOperations) {
     console.log('userOperation: ', action);
